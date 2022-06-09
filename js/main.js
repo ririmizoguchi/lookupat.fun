9b4b437169589b415a913afb79995ddb8e1b0bf7
ririmizoguchi <ririmizoguchi2003@gmail.com>
2022-06-10 03:23:44 +0900

    進捗

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+  var targetElement = $('.fadein').offset().top;
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if(scroll > targetElement - windowHeight+200){
+      $('.fadein').css('opacity','1');
+      $('.fadein').css('transform', 'translateY(0)');
+  }
+});
