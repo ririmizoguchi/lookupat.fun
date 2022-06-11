a9c1cec2cdc2c8d4c62eca248c261f03e9332711
ririmizoguchi <ririmizoguchi2003@gmail.com>
2022-06-12 05:00:40 +0900

    fadein and border animation実装

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-$(window).scroll(function(){
-    var targetElement = $('.fadein').offset().top;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if(scroll > targetElement - windowHeight+200){
-      $('.fadein').css('opacity','1');
-      $('.fadein').css('transform', 'translateY(0)')
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  $(".fadein").each(function () {
+    var targetPosition = $(this).offset().top;
+    if (scroll > targetPosition - windowHeight + 100) {
+      $(this).css('opacity', '1');
+      $(this).css('transform', 'translateY(0)');
+
     }
-      });
+
+  })
+  var borderAnimeTarget = $('.borderAnimeTarget').offset().top;
+  if (scroll > borderAnimeTarget - windowHeight + 100) {
+    $(".borderAnimeTarget").addClass('current');
+  }
+
+});
