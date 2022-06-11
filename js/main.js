27b19bcb73248075ab6e5cb730cd4430d9e0aa80
ririmizoguchi <ririmizoguchi2003@gmail.com>
2022-06-12 02:59:04 +0900

    put img

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$(window).scroll(function () {
-  var targetElement = $('.fadein').offset().top;
-  var scroll = $(window).scrollTop();
-  var windowHeight = $(window).height();
-  if(scroll > targetElement - windowHeight+200){
+$(window).scroll(function(){
+    var targetElement = $('.fadein').offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if(scroll > targetElement - windowHeight+200){
       $('.fadein').css('opacity','1');
-      $('.fadein').css('transform', 'translateY(0)');
-  }
-});
+      $('.fadein').css('transform', 'translateY(0)')
+    }
+      });
